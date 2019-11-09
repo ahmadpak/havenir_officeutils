c500468267724e1746494cd389e2ed682b8658ae
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-11-09 15:40:59 +0500

    payment entry bug fix

--- havenir_officeutils/utils/payment_entry.py
+++ havenir_officeutils/utils/payment_entry.py
@@ -3,7 +3,7 @@ from frappe.utils import flt, get_datetime, getdate, add_days, date_diff, nowdat
 from frappe.model.document import Document
 
 def update_customer(self,cdt):
-    if self.party_type == 'Customer':
+    if self.party_type == 'Customer' and self.payment_type == 'Receive':
         customer = frappe.get_doc('Customer',self.party)
         customer.db_set('last_payment_date',self.posting_date)
         customer.db_set('last_payment_amount',self.paid_amount)
